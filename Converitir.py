a1053cd4721d38d335481d06e0fa9ebf97fcfc95
CristhianIncaluque <81604070+CristhianIncaluque@users.noreply.github.com>
2021-04-23 20:28:18 -0500

    Update Converitir.py

--- Converitir.py
+++ Converitir.py
@@ -2,7 +2,7 @@ a = 14
 b = 12
 c = a + b
 print (a, "hola", b, "=", c)
-#print (a + "hola" + b + "#" + c)
+#print (a + "hola" + b + "=" + c)
 
 #Al momento de concatenar numero y cadenas de texto es necesario convertir numeros a variables de texto
 
